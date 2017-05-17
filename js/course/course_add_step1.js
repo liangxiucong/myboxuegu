62d1a54e413739841c919152e808315ad02c87bd
cloverliang <617538859@qq.com>
2017-05-17 20:07:10 +0800

    5.17第二次提交

--- js/course/course_add_step1.js
+++ js/course/course_add_step1.js
@@ -2,25 +2,26 @@
 * @Author: cloverliang
 * @Date:   2017-05-12 00:23:28
 * @Last Modified by:   cloverliang
-* @Last Modified time: 2017-05-17 01:14:29
+* @Last Modified time: 2017-05-17 16:09:29
 */
 /**
  * Created by cloverliang on 2017/5/15.
  */
-define(['header','aside','util','nprogress','jquery_form','jquery','template'],function(ud,ud,util,nprogress,us,$,template){
+define(['header','aside','util','nprogress','jquery_form','jquery','template'],function(ud,ud,util,nprogress,ud,$,template){
     //util返回每一个方法的返回值，想用哪个就用哪个
     var returns=util({
        'checkLoginStatus':[],
        'loading':[],
        'getSearch':['cs_id']
-       
     });
-    var cs_id = returns.getSearch;
-    $.get('/v6/course/basic',{cs_id:cs_id},function(res){
-        console.log(res)
-        var data  = res.result;
-        $('.content').html(template('content-tpl',data))
-    })
+
+
+    // var cs_id = returns.getSearch;
+    // $.get('/v6/course/basic',{cs_id:cs_id},function(res){
+    //     console.log(res)
+    //     var data  = res.result;
+    //     $('.content').html(template('content-tpl',data))
+    // })
     // $.ajax({
     //     url: '/v6/course/basic',
     //     type: 'get',
@@ -39,11 +40,48 @@ define(['header','aside','util','nprogress','jquery_form','jquery','template'],f
     //     }
     // })
 
-    $('.bu2').on('click',function(){
-       loation.href = './course_add_step2.html?cs_id='+cs_id
-    })
-    $('.bu3').on('click',function(){
-       loation.href = './course_add_step3.html?cs_id='+cs_id
-    })
+
+    // $('.bu2').on('click',function(){
+    //    loation.href = './course_add_step2.html?cs_id='+cs_id
+    // })
+    // $('.bu3').on('click',function(){
+    //    loation.href = './course_add_step3.html?cs_id='+cs_id
+    // })
     
+
+     //课程基本信息数据回显
+     var cs_id=returns.getSearch;
+     $.get('/v6/course/basic',{cs_id:cs_id},function(data){
+        $('.steps').html(template('steps1-tpl',data.result));
+        steps1Submit();
+     });
+
+/*
+课程基本信息数据提交
+ */
+function steps1Submit(){
+    $('form.basic').ajaxForm(function(data){
+        loation.href='/html/course/course_add_step2.html?cs_id=' + cs_id;
+    });
+}
+
+/**
+     * 课程分类二级联动：
+     * 通过委托的方式监听顶级分类值的变化，
+     * 当发生变化时获取被选中的顶级分类ID，
+     * 然后利用这个ID发送请求获取该顶级分类下的子级分类，
+     * 然后动态生成一堆options标签进行子级分类列表的替换。
+     * */
+$(document).on('change','#top-cg-select',function(){
+    $.get('/v6/category/child',{cg_id:$(this).val()},function(data){
+        var options='';
+        for(var i=0,len=data.result.length;i<len;i++){
+            options+='<option value="' + data.result[i].cg_id + '">' + data.result[i].cg_name + '</option>';
+        }
+        $('#child-cg-select').html(options);
+    });
+});
+
+    // 销毁网站加载进度条
+    nprogress.done();
 });
--- js/course/course_add_step2.js
+++ js/course/course_add_step2.js
@@ -2,42 +2,81 @@
 * @Author: cloverliang
 * @Date:   2017-05-12 00:23:28
 * @Last Modified by:   cloverliang
-* @Last Modified time: 2017-05-17 00:11:29
+* @Last Modified time: 2017-05-17 16:44:52
 */
 
 
-define(['header','aside','util','nprogress','jquery_form','jquery'],function(ud,ud,util,nprogress,us,$){
+define(['header', 'aside', 'util', 'nprogress', 'jquery_uploadify', 'jquery', 'template'], function(ud, ud, util, nprogress, ud, $, template) {
     //util返回每一个方法的返回值，想用哪个就用哪个
     var returns=util({
        'checkLoginStatus':[],
        'loading':[],
        'getSearch':['cs_id']
-       
     });
-    console.log(returns)
-    $.ajax({
-        url: '/v6/course/picture',
-        type: 'get',
-        dataType: 'json',
-        data: {cs_id: returns.getSearch},
-        success:function(res){
-            console.log(res)
-            if( res.code != 200 ){
-            alert('后台数据发生错误');
-            return
-        }
-        var data = res.result;
-        },
-        error:function(res){
-            alert('数据请求失败')
-        }
-    })
 
-    $('.bu1').on('click',function(){
-       loation.href = './course_add_step1.html?cs_id='+returns.getSearch
-    })
-    $('.bu3').on('click',function(){
-       loation.href = './course_add_step3.html?cs_id='+returns.getSearch
-    })
+    // console.log(returns)
+    // $.ajax({
+    //     url: '/v6/course/picture',
+    //     type: 'get',
+    //     dataType: 'json',
+    //     data: {cs_id: returns.getSearch},
+    //     success:function(res){
+    //         console.log(res)
+    //         if( res.code != 200 ){
+    //         alert('后台数据发生错误');
+    //         return
+    //     }
+    //     var data = res.result;
+    //     },
+    //     error:function(res){
+    //         alert('数据请求失败')
+    //     }
+    // })
+
+    // $('.bu1').on('click',function(){
+    //    loation.href = './course_add_step1.html?cs_id='+returns.getSearch
+    // })
+    // $('.bu3').on('click',function(){
+    //    loation.href = './course_add_step3.html?cs_id='+returns.getSearch
+    // })
     
+/**
+     * 课程图片数据回显
+     * */
+    var cs_id = returns.getSearch;
+    $.get('/v6/course/picture', { cs_id: cs_id }, function(data){
+        $('.steps').html(template('steps2-tpl', data.result));
+        initUploadify();
+    });
+    
+    /**
+     * 初始化图片上传插件
+     * */
+    function initUploadify() {
+        $('#uploadify').uploadify({
+            swf: '/lib/uploadify/uploadify.swf',  // flash选取文件的脚本
+            uploader: '/v6/uploader/cover', // 接口
+            fileObjName: 'cs_cover_original', // 相当于表单的name属性
+            formData: {                      // 除了文件额外提交的数据
+                cs_id: cs_id
+            },
+            buttonText: '上传图片',
+            buttonClass: 'btn btn-success btn-sm btn-uploadify',
+            height: 30,
+            width: 80,
+            itemTemplate: '<i></i>',  
+            onUploadSuccess: function(file, data) {
+                try {
+                    var data = JSON.parse(data);
+                    $('.preview img').attr('src', data.result.path);
+                    $('.thumb img').attr('src', data.result.path);
+                    location.href = '/html/course/course_add_step3.html?cs_id=' + cs_id;
+                }catch(e){}
+            }
+        });
+    }
+
+
+// 销毁网站加载进度条
+    nprogress.done();
 });
